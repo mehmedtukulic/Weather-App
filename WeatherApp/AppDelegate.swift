d7c71aec9e5167cc8e7f25d9124915904857e9b9
Mehmed Tukulic <mehmed.tukulic@hotmail.com>
2021-04-17 23:05:12 +0200

    Inital project setup

---
+++ WeatherApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Mehmed Tukulic on 17/04/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ WeatherApp/Scenes/Initial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Mehmed Tukulic on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
